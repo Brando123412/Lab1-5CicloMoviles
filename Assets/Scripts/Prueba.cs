ccf6b8a68f16b67c6627f4cdaed9fd53338b437e
Brando123412 <130480267+Brando123412@users.noreply.github.com>
2024-03-12 21:07:08 -0500

    Avance1

--- Assets/Scripts/Prueba.cs
+++ Assets/Scripts/Prueba.cs
@@ -3,19 +3,46 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
-
+using System;
 public class Prueba : MonoBehaviour
 {
     private Vector2 pos;
     Touch primerTouch;
     [SerializeField] GameObject instanciaObjeto;
+    [SerializeField] Vector3 positionSpanw;
+    Vector3 touchPosition;
 
+    [SerializeField] bool isTouching = false;
+    [SerializeField] float timer = 0;
     void Update()
     {
-        if (Input.touchCount >0)
+        if (Input.touchCount > 0)
         {
-            primerTouch = Input.GetTouch(0);
+            isTouching = true;
         }
+        else
+        {
+            isTouching = false;
+        }
+        
+        if (isTouching)
+        {
+            timer = timer + Time.deltaTime;
+            if(timer > 1f)
+            {
+                VerifyRay();
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+        /*
+         * 
+         * 
+            
+            Instantiate(instanciaObjeto,new Vector3(positionSpanw.x,positionSpanw.y,0),Quaternion.identity);
+         */
     }
     public void CambioColor(GameObject color)
     {
@@ -24,5 +51,28 @@ public class Prueba : MonoBehaviour
     public void CambioImagen(GameObject imagen)
     {
         instanciaObjeto.GetComponent<SpriteRenderer>().sprite = imagen.GetComponent<Image>().sprite;
+    }
+    void VerifyRay()
+    {
+        primerTouch = Input.GetTouch(0);
+        touchPosition = Camera.main.ScreenToWorldPoint(primerTouch.position);
+
+        //Debug.DrawRay(touchPosition, Vector2.up , UnityEngine.Color.green, 1f);
+        RaycastHit2D hit = Physics2D.Raycast(touchPosition, Vector2.zero);
+        //puedes mover
+
+        if (hit.collider != null )
+        {
+            //GameObject objetoGolpeado = hit.collider.gameObject;
+            Transform otherTransfomr = hit.collider.gameObject.transform;
+            otherTransfomr.position = new Vector3(touchPosition.x, touchPosition.y,0);
+        }
+
+    }
+    void PositionTouch()
+    {
+        primerTouch = Input.GetTouch(0);
+        touchPosition = primerTouch.position;
+        positionSpanw = Camera.main.ScreenToWorldPoint(touchPosition);
     }
 }
